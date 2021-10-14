1fad368a87578da46e58035f03cb1a48e8e9926d
Galileo Daras <howard@getcoffee.io>
2021-10-14 15:16:31 +0200

    Use more common naming convention (take_disk)

--- src/lib.rs
+++ src/lib.rs
@@ -620,9 +620,11 @@ impl<'a> GptDisk<'a> {
     }
 
     /// Take the underlying device object and force
-    /// self to drop.  
-    pub fn relinquish_device(self) -> DiskDeviceObject<'a> {
+    /// self to drop out of scope.
+    ///
+    /// Caution: this will abandon any writes or changes made
+    /// to GptDisk.
+    pub fn take_device(self) -> DiskDeviceObject<'a> {
         self.device
     }
-
 }
